0c35f0756a00518b0db11c60a93222b60bbc0795
WienWong <wangzichengfu@gmail.com>
2014-12-21 21:44:49 +0100

    Create assignment 6_5.py

---
+++ assignment 6_5.py
@@ -0,0 +1,15 @@
+'''
+6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert
+the extracted value to a floating point number and print it out.
+'''
+
+text = "X-DSPAM-Confidence:    0.8475"
+zeropos = text.find('0')
+print float(text[zeropos + 1: ])
+
+
+
+'''
+Desired Output:
+0.8475
+'''

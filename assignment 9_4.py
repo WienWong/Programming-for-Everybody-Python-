53eb32cf9df6f1d56b4ce48b7a1f789b905cfe6e
WienWong <wangzichengfu@gmail.com>
2014-12-21 22:32:30 +0100

    Create assignment 9_4.py

---
+++ assignment 9_4.py
@@ -0,0 +1,35 @@
+'''
+9.4 Write a program to read through the mbox-short.txt and figure out who has the sent the greatest number of mail messages. The
+program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a
+Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the
+dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+'''
+
+nfile = raw_input("Enter file:")
+if len(nfile) < 1: 
+  name = "mbox-short.txt"
+fhandle = open(nfile)
+
+counts = dict()
+
+for line in fhandle:
+  if line.startswith("From "):
+    words = line.split()
+    name = words[1]
+    counts[name] = counts.get(name, 0) + 1
+    
+maxCount = None
+maxEmail = None
+for email,count in counts.items():
+  if maxCount is None or maxCount < count:
+    maxCount = count
+    maxEmail = email
+
+print maxEmail, maxCount
+
+
+
+'''
+Desired Output:
+cwen@iupui.edu 5
+'''
